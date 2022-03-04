11d5ec1c27e012ccf279fe96494ecb2559c8210a
Melissa Astbury <melissa.astbury@footasylum.com>
2022-03-04 13:46:20 +0000

    completed kata in TDD style

---
+++ FizzBuzz.Tests/FizzBuzzCalculatorTests.cs
@@ -0,0 +1,44 @@
+﻿using Xunit;
+using Shouldly;
+
+namespace FizzBuzz.Tests
+{
+    public class FizzBuzzCalculatorTests
+    {
+        [Fact]
+        public void ShouldCreateNewFizzBuzzCalculator()
+        {
+            FizzBuzzCalculator calc = new();
+            calc.ShouldNotBeNull();
+            calc.ShouldBeOfType<FizzBuzzCalculator>();
+        }
+
+        [Theory]
+        [InlineData(1, "1")]
+        [InlineData(2, "2")]
+        [InlineData(3, "Fizz")]
+        [InlineData(4, "4")]
+        [InlineData(5, "Buzz")]
+        [InlineData(6, "Fizz")]
+        [InlineData(7, "7")]
+        [InlineData(8, "8")]
+        [InlineData(9, "Fizz")]
+        [InlineData(10, "Buzz")]
+        [InlineData(11, "11")]
+        [InlineData(12, "Fizz")]
+        [InlineData(13, "Fizz")]
+        [InlineData(14, "14")]
+        [InlineData(15, "FizzBuzz")]
+        [InlineData(16, "16")]
+        [InlineData(17, "17")]
+        [InlineData(18, "Fizz")]
+        [InlineData(19, "19")]
+        [InlineData(20, "Buzz")]
+        public void GivenNumberShouldPrintValidOutput(int num, string expectedResult)
+        {
+            FizzBuzzCalculator calc = new();
+            var result = calc.PrintNumber(num);
+            result.ShouldBe(expectedResult);
+        }
+    }
+}
---
+++ FizzBuzz/FizzBuzzCalculator.cs
@@ -0,0 +1,29 @@
+﻿namespace FizzBuzz
+{
+    public class FizzBuzzCalculator
+    {
+        public FizzBuzzCalculator()
+        {
+
+        }
+
+        public string PrintString(int num)
+        {
+            if (num % 3 == 0 && num % 5 == 0)
+            {
+                return "FizzBuzz";
+            }
+
+            if (num % 3 == 0 || num.ToString().Contains('3'))
+            {
+                return "Fizz";
+            }
+
+            if (num % 5 == 0)
+            {
+                return "Buzz";
+            }
+            return num.ToString();
+        }
+    }
+}
